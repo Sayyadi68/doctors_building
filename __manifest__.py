1be01d4a5256675d531dc6329b32f3e83e2b8af6
Mahdi sayyadi <taiyoh68@gmail.com>
2026-08-31 23:36:15 +0330

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Doctors Building',
+    'version': '17.0.1.0.0',
+    'category': 'خدماتی/مدیریتی',
+    'summary': 'سامانه مدیریت بیماران ساختمان پزشکان تهران',
+    'author': 'صیادی-فهیم-پورشهامی',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'application': True,
+    'installable': True,
+}
