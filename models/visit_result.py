e36d5d5ac0bc6568c7a498392cf275e3f7984015
Mahdi sayyadi <taiyoh68@gmail.com>
2026-09-02 12:44:13 +0330

    bug fix

--- models/visit_result.py
+++ models/visit_result.py
@@ -6,9 +6,9 @@ class VisitResult(models.Model):
     _description = 'VisitResult'
 
     
-    Doctor=fields.Many2many("doctors_building.Doctor",string="پزشک معالج")
+    Doctor=fields.Many2many("doctors_building.Doctors",string="پزشک معالج")
     Patient=fields.Many2many("doctors_building.Patient",string="نام بیمار")
     factor_consultation_fee = fields.Float(string="هزینه وزیت" , default="280000")
     Status = fields.Selection(string="وضعیت", required=True, selection=[('pending','در حال انجام'),('done','ویزیت انجام شده است'),('cancel','لغو شده')],)
-    is_active = fields.Char(default=True)
+    is_active = fields.Boolean(default=True)
 	
